cc63d840d916010d7aee3741beb6acf42dde0dd6
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-02-05 17:18:34 +0900

    add important options

--- src/constants/options/important.ts
+++ src/constants/options/important.ts
@@ -7,6 +7,10 @@ export const importantOptions: OptionType[] = [
   { value: "業界", label: "業界" },
   { value: "職種", label: "職種" },
   { value: "働きがい", label: "働きがい" },
+  { value: "安定性", label: "安定性" },
+  { value: "成長性", label: "成長性" },
+  { value: "休暇制度", label: "休暇制度" },
   { value: "福利厚生", label: "福利厚生" },
+  { value: "社会貢献度", label: "社会貢献度" },
   { value: "その他", label: "その他" },
 ];
